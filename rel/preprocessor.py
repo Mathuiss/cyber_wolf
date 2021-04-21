43ea95f75a876ecd61a3d6d026068aca7d9cc03e
Mathuiss <mathijsgeerlings@gmail.com>
2021-04-21 13:47:58 +0200

    Development feature extraction

--- rel/preprocessor.py
+++ rel/preprocessor.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy
 
 # Set data directory
-data_path = "../data"
+data_path = "/home/mathuis/Downloads/cyber_wolf/data"
 
 # Set feature definition
 feature_def = ["path", "header", "body", "length", "a", "b", "c", "d", "e", "f", "g", "h", "i",
@@ -19,35 +19,81 @@ def read_file_content(data_path):
         return f.readlines()
 
 
-def extract_features(df, request):
-    for i in range(len(request)):
-        values = get_values(df, request, i)
+def extract_features(request):
+    df = pd.DataFrame(columns=feature_def)
 
-        for value in values:
-            df = histogram(df, value)
+    for i in range(len(request)):
+        frames = get_values(request, i)
+        df = df.append(frames, ignore_index=True)
 
     return df
 
 
-def get_values(df, request, i):
-    # Set default values
-    index = len(df) + 1
-    row = df.loc[index]
-    row = 0
-
-    print(row)
-
+def get_values(request, i):
+    df = pd.DataFrame(columns=feature_def)
     line = request[i]
     print(line)
 
+    # In query
     if i == 0:
-        return split_query()
+        values = split_query(line)
+        df = df.append(histogram(values, "path"), ignore_index=True)
 
-    if line.contains(": ")
-    return split_header()
+    # In header
+    if line.contains(": "):
+        values = split_header(line)
+        df = df.append(histogram(values, "header"), ignore_index=True)
+
+    # In body
+    if line != "" and "" in request[:i]:
+        values = split_body(line)
+        df = df.append(histogram(values, "body"), ignore_index=True)
+
+    return df
 
 
-def histogram(df, values):
+def split_query(line):
+    if "?" not in line:
+        return []
+
+    querystring = line.split("?")[1]
+
+    if "&" not in querystring:
+        return list(querystring.split("=")[1])
+
+    values = []
+    sections = querystring.split("&")
+    for section in sections():
+        values.append(section.split("=")[1])
+
+    return values
+
+
+def split_header(line):
+    return list(line.split(": "[1]))
+
+
+def split_body(line):
+    if "&" not in line:
+        return list(line.split("=")[1])
+
+    values = []
+    parameters = line.split("&")
+    for param in parameters:
+        values.append(param.split("=")[1])
+
+
+def histogram(values, location):
+    for value in values:
+        df = pd.DataFrame(columns=feature_def)
+        # Set default values
+        df.loc[0] = 0
+        row = df.loc[0]
+        row["length"] = len(value)
+        row[location] = 1
+
+        for char in value:
+            row[char] += 1
 
     return df
 
@@ -61,7 +107,11 @@ def main():
         file_contents = read_file_content(f"{data_path}/{file}")
 
         # Extract features from contents
-        features = extract_features(df, file_contents)
+        features = extract_features(file_contents)
+
+        df = df.append(features, ignore_index=True)
+
+    print(df)
 
 
 if __name__ == "__main__":
